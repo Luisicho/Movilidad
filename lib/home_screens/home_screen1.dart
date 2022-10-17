c41988b71c1096c47d45674405ce987908ce61aa
luisicho <luism18.1999@gmail.com>
2022-10-16 20:57:21 -0500

    add Map Main, SpeedDial

--- lib/home_screens/home_screen1.dart
+++ lib/home_screens/home_screen1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../maps/map_main.dart';
 
 class HomeScreen1 extends StatefulWidget {
   const HomeScreen1({Key? key}) : super(key: key);
@@ -14,41 +15,43 @@ class _HomeScreen1State extends State<HomeScreen1> {
         decoration: const BoxDecoration(
           color: Color.fromARGB(255, 78, 0, 92),
           image: DecorationImage(
-            image: AssetImage(
-              "assets/fondo.png",
-            ),
-            fit: BoxFit.fill
-          ),
+              image: AssetImage(
+                "assets/fondo.png",
+              ),
+              fit: BoxFit.fill),
         ),
         child: Scaffold(
             backgroundColor: Colors.transparent,
             body: Center(
-                child: InkWell(
-                  splashColor: Colors.black26,
-                  onTap: () {},
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Ink.image(
+              child: InkWell(
+                splashColor: Colors.black26,
+                onTap: () {
+                  goToMain();
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Ink.image(
                         image: const AssetImage(
                           "assets/mapicon.png",
                         ),
                         height: 400,
                         width: 400,
-                        fit: BoxFit.contain
-                      ),
-                      const Text(
-                        'Maps & GO',
-                        style: TextStyle(
-                          fontSize: 32,
-                          color: Colors.white
-                        ),
-                      ),
-                    ],
-                  ),
+                        fit: BoxFit.contain),
+                    const Text(
+                      'Maps & GO',
+                      style: TextStyle(fontSize: 32, color: Colors.white),
+                    ),
+                  ],
                 ),
+              ),
             )
-        )
-    );
+          )
+        );
+  }
+
+  void goToMain() {
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const MapMain()));
   }
 }
