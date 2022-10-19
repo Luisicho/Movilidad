c0b8e93f4e650b1bda616b14d360dd04506dc38f
luisicho <luism18.1999@gmail.com>
2022-10-19 15:15:17 -0500

    Hora y fecha estaticas

--- lib/levantamiento/lev_main.dart
+++ lib/levantamiento/lev_main.dart
@@ -26,157 +26,193 @@ class _levMainState extends State<levMain> {
   final TextEditingController userController = new TextEditingController();
   final TextEditingController hourController = new TextEditingController();
 
-  //Variables extras
-
   @override
   Widget build(BuildContext context) {
+    //Inicio de variables
+    //ASIGNA HORA DE HOY
+    hourController.text = TimeOfDay.now().format(context).toString();
+    //ASIGNA DIA DE HOY
+    fechaController.text = DateFormat("dd-MM-yyyy").format(DateTime.now());
+    
+    
     //--------------Object Varaible-----------------------
-    //user field
-    final usuarioField = TextFormField(
-      autofocus: false,
-      controller: userController,
-      validator: (value) {
-        if (value!.isEmpty) {
-          return ("El Usuario es requerido para ingresar");
-        }
-      },
-      onSaved: (value) {
-        userController.text = value!;
-      },
-      textInputAction: TextInputAction.done,
-      decoration: InputDecoration(
-        prefixIcon: const Icon(Icons.person),
-        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-        hintText: "Usuario",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
+    //Folio Field
+    final folioField = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('No. Folio'),
+        const SizedBox(width: 30),
+        Expanded(
+          flex: 2,
+          child: TextFormField(
+              focusNode: folioFocus,
+              controller: folioController,
+              readOnly: true,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.pages),
+                contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                hintText: "Folio",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              )),
         ),
-      ),
+      ],
+    );
+
+    //user field
+    final usuarioField = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Usuario'),
+        const SizedBox(width: 30),
+        Expanded(
+          flex: 2,
+          child: TextFormField(
+            autofocus: false,
+            controller: userController,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return ("El Usuario es requerido para ingresar");
+              }
+            },
+            onSaved: (value) {
+              userController.text = value!;
+            },
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.person),
+              contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+              hintText: "Usuario",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
 
     //DateField
-    final dateField = TextFormField(
-      readOnly: true,
-      controller: fechaController,
-      onFieldSubmitted: (value) {
-        FocusScope.of(context).requestFocus(hourFocus);
-      },
-      decoration: InputDecoration(
-        prefixIcon: const Icon(Icons.calendar_today),
-        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-        hintText: "Fecha",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
+    final dateField = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('No. Folio'),
+        const SizedBox(width: 30),
+        Expanded(
+          flex: 2,
+          child: TextFormField(
+            readOnly: true,
+            controller: fechaController,
+            onFieldSubmitted: (value) {
+              FocusScope.of(context).requestFocus(hourFocus);
+            },
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.calendar_today),
+              contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+              hintText: "Fecha",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            onTap: () async {
+              // Get the date
+              // DateTime? datePicket = await showDatePicker(
+              //   context: context,
+              //   initialDate: DateTime.now(),
+              //   firstDate: DateTime(1999),
+              //   lastDate: DateTime(2050),
+              // );
+              // if (datePicket != null) {
+              //   //make format to the date
+              //   var formatedDate = DateFormat("dd-MM-yyyy").format(datePicket);
+              //   fechaController.text = formatedDate.toString();
+              // }
+            },
+          ),
         ),
-      ),
-      onTap: () async {
-        // Get the date
-        DateTime? datePicket = await showDatePicker(
-          context: context,
-          initialDate: DateTime.now(),
-          firstDate: DateTime(1999),
-          lastDate: DateTime(2050),
-        );
-        if (datePicket != null) {
-          //make format to the date
-          var formatedDate = DateFormat("dd-MM-yyyy").format(datePicket);
-          fechaController.text = formatedDate.toString();
-        }
-      },
+      ],
     );
 
     //HourField
-    final hourField = TextFormField(
-      readOnly: true,
-      controller: hourController,
-      onFieldSubmitted: (value) {
-        FocusScope.of(context).requestFocus(userFocus);
-      },
-      decoration: InputDecoration(
-        prefixIcon: const Icon(Icons.access_alarm),
-        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-        hintText: "Hora",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
+    final hourField = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Hora llegada'),
+        const SizedBox(width: 30),
+        Expanded(
+          flex: 2,
+          child: TextFormField(
+            readOnly: true,
+            controller: hourController,
+            onFieldSubmitted: (value) {
+              FocusScope.of(context).requestFocus(userFocus);
+            },
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.access_alarm),
+              contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+              hintText: "Hora",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            onTap: () async {
+              // Get the date
+              //TimeOfDay? timePicket = await showTimePicker(context: context,initialTime: TimeOfDay.now(),);
+              //if (timePicket != null) {
+                //make format to the time
+                //hourController.text = timePicket.format(context).toString();
+              //}
+            },
+          ),
         ),
-      ),
-      onTap: () async {
-        // Get the date
-        TimeOfDay? timePicket = await showTimePicker(
-          context: context,
-          initialTime: TimeOfDay.now(),
-        );
-        if (timePicket != null) {
-          //make format to the time
-          //var formatedDate = DateFormat("dd-MM-yyyy").format(datePicket);
-          hourController.text = timePicket.format(context).toString();
-        }
-      },
+      ],
     );
+
     //--------------Object Varaible-----------------------
 
-    return Scaffold(
-      body: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('No. Folio'),
-                  const SizedBox(width: 30),
-                  SizedBox(
-                    width: 200,
-                    child: TextFormField(
-                        focusNode: folioFocus,
-                        controller: folioController,
-                        readOnly: true,
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(Icons.pages),
-                          contentPadding:
-                              const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                          hintText: "Folio",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        )),
-                  ),
-                ],
-              ),
-              dateField,
-              hourField,
-              usuarioField,
-              TextButton(
-                child: Text('Posicion Actual'),
-                onPressed: () async {
-                  //Pedir permiso para posicion actual
-                  LocationPermission permission =
-                      await Geolocator.requestPermission();
-                  //Consigue posicion actual
-                  Position _currentPosition =
-                      await Geolocator.getCurrentPosition(
-                          desiredAccuracy: LocationAccuracy.high);
-                  String mesg = "Lon " +
-                      _currentPosition.longitude.toString() +
-                      " Lat " +
-                      _currentPosition.latitude.toString();
-                  Fluttertoast.showToast(
-                    msg: mesg,
-                    toastLength: Toast.LENGTH_SHORT,
-                    gravity: ToastGravity.CENTER,
-                    timeInSecForIosWeb: 1,
-                    backgroundColor: Colors.red,
-                    textColor: Colors.white,
-                    fontSize: 16.0
-                  );
-                },
-              ),
-            ],
-          )),
-    );
     //------------------Functions-----------------
 
     //------------------Functions-----------------
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.all(20.0),
+        child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                folioField,
+                dateField,
+                hourField,
+                usuarioField,
+                TextButton(
+                  child: const Text('Posicion Actual'),
+                  onPressed: () async {
+                    //Pedir permiso para posicion actual
+                    LocationPermission permission =
+                        await Geolocator.requestPermission();
+                    //Consigue posicion actual
+                    Position _currentPosition =
+                        await Geolocator.getCurrentPosition(
+                            desiredAccuracy: LocationAccuracy.high);
+                    String mesg = "Lon " +
+                        _currentPosition.longitude.toString() +
+                        " Lat " +
+                        _currentPosition.latitude.toString();
+                    Fluttertoast.showToast(
+                        msg: mesg,
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.CENTER,
+                        timeInSecForIosWeb: 1,
+                        backgroundColor: Colors.red,
+                        textColor: Colors.white,
+                        fontSize: 16.0);
+                  },
+                ),
+              ],
+            )),
+      ),
+    );
   } //widget
 }
