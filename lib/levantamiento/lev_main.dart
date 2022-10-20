f78e7ab6acff1ec8fecf4e660f15424254fa342f
luisicho <lumihernadezma@ittepic.edu.mx>
2022-10-20 11:45:43 -0600

    Nuevo Stepp, remove bug

--- lib/levantamiento/lev_main.dart
+++ lib/levantamiento/lev_main.dart
@@ -12,32 +12,44 @@ class levMain extends StatefulWidget {
 }
 
 class _levMainState extends State<levMain> {
-
   // form key avisa los cambios
   final _formKey = GlobalKey<FormState>();
 
   // Focus Nodes
+  //Step1
   final folioFocus = FocusNode();
   final fechaFocus = FocusNode();
   final hourFocus = FocusNode();
   final userFocus = FocusNode();
+  //Step2
 
   // editing controller
+  //Step1
   final TextEditingController folioController = TextEditingController();
   final TextEditingController fechaController = TextEditingController();
   final TextEditingController userController = TextEditingController();
   final TextEditingController hourController = TextEditingController();
-  
+  //Step2
+
+  //-----------Inicio de variables y controladores------------------
+  int currStep = 0;
+
+  //-----Iniciar la ventana
+  @override
+  void initState() {
+    
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    //Inicio de variables
-    int currStep = 0;
     //ASIGNA HORA DE HOY
     hourController.text = TimeOfDay.now().format(context).toString();
     //ASIGNA DIA DE HOY
     fechaController.text = DateFormat("dd-MM-yyyy").format(DateTime.now());
 
     //--------------Object Varaible-----------------------
+    //--------------Step No1
     //Folio Field
     final folioField = Row(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -149,68 +161,94 @@ class _levMainState extends State<levMain> {
       ],
     );
 
+    //
+    //--------------Step No1
     //--------------Object Varaible-----------------------
 
     //------------------Functions-----------------
     List<Step> getSteps() => [
-       Step(
-        state: currStep > 0 ? StepState.complete : StepState.indexed,
-        isActive: currStep >= 0,
-        title: const Text('Accidende'), 
-        content: Container(
-          height: 500,
-          margin: const EdgeInsets.all(10.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                folioField,
-                dateField,
-                hourField,
-                usuarioField,
-                TextButton(
-                  child: const Text('Posicion Actual'),
-                  onPressed: () async {
-                    //Pedir permiso para posicion actual
-                    LocationPermission permission =
-                        await Geolocator.requestPermission();
-                    //Consigue posicion actual
-                    Position _currentPosition =
-                        await Geolocator.getCurrentPosition(
-                            desiredAccuracy: LocationAccuracy.high);
-                    String mesg = "Lon " +
-                        _currentPosition.longitude.toString() +
-                        " Lat " +
-                        _currentPosition.latitude.toString();
-                    Fluttertoast.showToast(
-                        msg: mesg,
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.CENTER,
-                        timeInSecForIosWeb: 1,
-                        backgroundColor: Colors.red,
-                        textColor: Colors.white,
-                        fontSize: 16.0);
-                  },
+          Step(
+            state: currStep > 0 ? StepState.complete : StepState.indexed,
+            isActive: currStep >= 0,
+            title: const Text('Accidende'),
+            content: Container(
+              height: 500,
+              margin: const EdgeInsets.all(10.0),
+              child:
+                //------------Formulario
+                Form(
+                key: _formKey,
+                child:
+                  //------------ColumnaForm
+                  Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    folioField,
+                    dateField,
+                    hourField,
+                    usuarioField,
+                    TextButton(
+                      child: const Text('Posicion Actual'),
+                      onPressed: () async{
+                        //Pedir permiso para posicion actual
+                        LocationPermission permission = await Geolocator.requestPermission();
+                        //Consigue posicion actual
+                        Position _currentPosition = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+                        String mesg = "Lon " +_currentPosition.longitude.toString() + " Lat " + _currentPosition.latitude.toString();
+                        //-------------Toast
+                        Fluttertoast.showToast(
+                            msg: mesg,
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.red,
+                            textColor: Colors.white,
+                            fontSize: 16.0);
+                        //-------------Toast
+                      },
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
-      Step(
-        state: currStep > 1 ? StepState.complete : StepState.indexed,
-        isActive: currStep >= 1,
-        title: const Text('Afectados'), 
-        content: const SizedBox(
-          height: 500,
-          child: Text('data'),
-        ),
-      ),
-    ];
+          Step(
+            state: currStep > 1 ? StepState.complete : StepState.indexed,
+            isActive: currStep >= 1,
+            title: const Text('Afectados'),
+            content: const SizedBox(
+              height: 500,
+              child: Text('data'),
+            ),
+          ),
+          Step(
+            state: currStep > 2 ? StepState.complete : StepState.indexed,
+            isActive: currStep >= 2,
+            title: const Text('Completado'),
+            content: SizedBox(
+              height: 500,
+              child: TextButton(
+                onPressed: () {
+                  //-------------Toast
+                  Fluttertoast.showToast(
+                      msg: 'Enviando a la nuve',
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.red,
+                      textColor: Colors.white,
+                      fontSize: 16.0);
+                  //-------------Toast
+                }, 
+                child: Text('Guardar'),
+              ),
+            ),
+          ),
+        ];
     //------------------Functions-----------------
 
-    //return
+    //----return
     return Scaffold(
       body: Stepper(
         type: StepperType.horizontal,
@@ -221,10 +259,15 @@ class _levMainState extends State<levMain> {
             currStep++;
           });
         },
-        onStepCancel: currStep == 0 ? null : () => setState(() {
-          currStep--;
-        }),
+        onStepCancel: currStep == 0
+            ? null
+            : () => setState(() {
+                  currStep--;
+                }),
       ),
     );
   } //widget
 }
+
+void iniComponents(BuildContext context, TextEditingController fechaController,
+    TextEditingController hourController) async {}
