36849d026f1d62528f30e2161792d4fa0e9de13a
luisicho <lumihernadezma@ittepic.edu.mx>
2022-10-27 12:01:04 -0600

    AddAltaField

--- lib/levantamiento/views/afectado_view.dart
+++ lib/levantamiento/views/afectado_view.dart
@@ -19,8 +19,12 @@ final TextEditingController vigenciaController = TextEditingController();
 final TextEditingController nombreAcController = TextEditingController();
 final TextEditingController CURPController = TextEditingController();
 final TextEditingController domicilioController = TextEditingController();
-final TextEditingController fechaController = TextEditingController();
-final TextEditingController hourController = TextEditingController();
+final TextEditingController fechaRecepcionController = TextEditingController();
+final TextEditingController hourRecepcionController = TextEditingController();
+final TextEditingController fechaAltaController = TextEditingController();
+final TextEditingController hourAltaController = TextEditingController();
+
+
 
 //Generales
 var initialList = "item1";
@@ -216,7 +220,7 @@ class _afectadoViewState extends State<afectadoView> {
           flex: 2,
           child: TextFormField(
             readOnly: true,
-            controller: fechaController,
+            controller: fechaRecepcionController,
             decoration: InputDecoration(
               prefixIcon: const Icon(Icons.calendar_today),
               contentPadding: const EdgeInsets.fromLTRB(05, 0, 05, 0),
@@ -236,7 +240,7 @@ class _afectadoViewState extends State<afectadoView> {
               if (datePicket != null) {
                 //make format to the date
                 var formatedDate = DateFormat("dd-MM-yyyy").format(datePicket);
-                fechaController.text = formatedDate.toString();
+                fechaRecepcionController.text = formatedDate.toString();
               }
             },
           ),
@@ -257,7 +261,7 @@ class _afectadoViewState extends State<afectadoView> {
           flex: 2,
           child: TextFormField(
             readOnly: true,
-            controller: hourController,
+            controller: hourRecepcionController,
             decoration: InputDecoration(
               prefixIcon: const Icon(Icons.access_alarm),
               contentPadding: const EdgeInsets.fromLTRB(05, 0, 05, 0),
@@ -275,7 +279,7 @@ class _afectadoViewState extends State<afectadoView> {
               if (timePicket != null) {
                 //make format to the time
                 //var formatedDate = DateFormat("dd-MM-yyyy").format(datePicket);
-                hourController.text = timePicket.format(context).toString();
+                hourRecepcionController.text = timePicket.format(context).toString();
               }
             },
           ),
@@ -300,6 +304,105 @@ class _afectadoViewState extends State<afectadoView> {
         ]
     );
 
+    //DateAltaField
+    final dateAltaField = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Text("Fecha Alta"),
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          flex: 2,
+          child: TextFormField(
+            readOnly: true,
+            controller: fechaAltaController,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.calendar_today),
+              contentPadding: const EdgeInsets.fromLTRB(05, 0, 05, 0),
+              hintText: "Fecha",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            onTap: () async {
+              // Get the date
+              DateTime? datePicket = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1999),
+                lastDate: DateTime(2050),
+              );
+              if (datePicket != null) {
+                //make format to the date
+                var formatedDate = DateFormat("dd-MM-yyyy").format(datePicket);
+                fechaAltaController.text = formatedDate.toString();
+              }
+            },
+          ),
+        ),
+      ],
+    );
+
+    //HourAltaField
+    final hourAltaField = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Text("Hora"),
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          flex: 2,
+          child: TextFormField(
+            readOnly: true,
+            controller: hourAltaController,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.access_alarm),
+              contentPadding: const EdgeInsets.fromLTRB(05, 0, 05, 0),
+              hintText: "Hora",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            onTap: () async {
+              // Get the date
+              TimeOfDay? timePicket = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              if (timePicket != null) {
+                //make format to the time
+                //var formatedDate = DateFormat("dd-MM-yyyy").format(datePicket);
+                hourAltaController.text = timePicket.format(context).toString();
+              }
+            },
+          ),
+        ),
+      ],
+    );
+
+    //AltaField
+    final AltaField =
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center, 
+        children: [
+          Expanded(
+            flex: 2,
+            child: dateAltaField
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            flex: 1,
+            child: hourAltaField
+          ),
+        ]
+    );
+
+
+
     //----Return
     return Container(
       height: 800,
@@ -322,6 +425,8 @@ class _afectadoViewState extends State<afectadoView> {
           domicilioField,
           const SizedBox(height: 10),
           RecepcionField,
+          const SizedBox(height: 10),
+          AltaField,
           //--------------Pruebas Flutter
           TextButton(
             onPressed: () {
