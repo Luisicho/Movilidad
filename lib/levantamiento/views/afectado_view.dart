01e7ac6fd00893de1e2ffd3ffc5a6d85d8fa52cc
luisicho <lumihernadezma@ittepic.edu.mx>
2022-10-27 11:30:31 -0600

    addAseguradoraField,FechaRecField, controllers

--- lib/levantamiento/views/afectado_view.dart
+++ lib/levantamiento/views/afectado_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
 
 class afectadoView extends StatefulWidget {
   @override
@@ -9,12 +10,16 @@ class afectadoView extends StatefulWidget {
 //-----------Variables globales
 // Focus Nodes
 //Step2
-final noLicenciaFocus = FocusNode();
+final hourFocus = FocusNode();
 
 // editing controller
 //Step2
 final TextEditingController polizaController = TextEditingController();
 final TextEditingController vigenciaController = TextEditingController();
+final TextEditingController nombreAcController = TextEditingController();
+final TextEditingController CURPController = TextEditingController();
+final TextEditingController domicilioController = TextEditingController();
+final TextEditingController fechaController = TextEditingController();
 
 //Generales
 var initialList = "item1";
@@ -83,7 +88,7 @@ class _afectadoViewState extends State<afectadoView> {
       Expanded(
         flex: 3,
         child: TextFormField(
-          controller: vigenciaController,
+          controller: nombreAcController,
           decoration: InputDecoration(
             prefixIcon: const Icon(Icons.ad_units_rounded),
             contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
@@ -108,7 +113,7 @@ class _afectadoViewState extends State<afectadoView> {
       Expanded(
         flex: 4,
         child: TextFormField(
-          controller: vigenciaController,
+          controller: CURPController,
           decoration: InputDecoration(
             prefixIcon: const Icon(Icons.ad_units_rounded),
             contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
@@ -133,7 +138,7 @@ class _afectadoViewState extends State<afectadoView> {
       Expanded(
         flex: 4,
         child: TextFormField(
-          controller: vigenciaController,
+          controller: domicilioController,
           decoration: InputDecoration(
             prefixIcon: const Icon(Icons.ad_units_rounded),
             contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
@@ -147,94 +152,139 @@ class _afectadoViewState extends State<afectadoView> {
     ],
   );
 
-
-
   @override
   Widget build(BuildContext context) {
+    //---------------Variables Locales Widget
+    //aseguradoraField
+    final aseguradoraField = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Text("Aseguradora"),
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+            flex: 2,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                border: Border.all(color: Colors.black38, width: 1),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                child: DropdownButton(
+                  // Initial Value
+                  value: initialList,
+
+                  // Down Arrow Icon
+                  icon: const Icon(Icons.keyboard_arrow_down),
+
+                  // Array list of items
+                  items: lista.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Text(items),
+                    );
+                  }).toList(),
+
+                  // After selecting the desired option,it will
+                  // change button value to selected value
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      initialList = newValue!;
+                    });
+                  },
+                ),
+              ),
+            )),
+      ],
+    );
+
+    //DateField
+    final dateField = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Text("Fecha Recepción"),
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+            flex: 2,
+            child: TextFormField(
+              readOnly: true,
+              controller: fechaController,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.calendar_today),
+                contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                hintText: "Fecha",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onTap: () async {
+                // Get the date
+                DateTime? datePicket = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1999),
+                  lastDate: DateTime(2050),
+                );
+                if (datePicket != null) {
+                  //make format to the date
+                  var formatedDate = DateFormat("dd-MM-yyyy").format(datePicket);
+                  fechaController.text = formatedDate.toString();
+                }
+              },
+            ),
+        ),
+      ],
+    );
+
+
+    //----Return
     return Container(
       height: 800,
       padding: EdgeInsets.all(10),
       child:
-        //---------------Column
-        Column(
-          children: [
-            //--AseguradoraField
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Text("Aseguradora"),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                      flex: 2,
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          border: Border.all(color: Colors.black38, width: 1),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
-                          child: DropdownButton(
-                            // Initial Value
-                            value: initialList,
+          //---------------Column
+          Column(
+        children: [
+          const SizedBox(height: 10),
+          aseguradoraField,
+          const SizedBox(height: 10),
+          polizaField,
+          const SizedBox(height: 10),
+          vigenciaField,
+          const SizedBox(height: 10),
+          nombreAcField,
+          const SizedBox(height: 10),
+          CURPField,
+          const SizedBox(height: 10),
+          domicilioField,
+          const SizedBox(height: 10),
+          dateField,
 
-                            // Down Arrow Icon
-                            icon: const Icon(Icons.keyboard_arrow_down),
-
-                            // Array list of items
-                            items: lista.map((String items) {
-                              return DropdownMenuItem(
-                                value: items,
-                                child: Text(items),
-                              );
-                            }).toList(),
-
-                            // After selecting the desired option,it will
-                            // change button value to selected value
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                initialList = newValue!;
-                              });
-                            },
-                          ),
-                        ),
-                      )),
-                ],
-              ),
-            ),
-            //------AseguradoraField
-            const SizedBox(height: 10),
-            polizaField,
-            const SizedBox(height: 10),
-            vigenciaField,
-            const SizedBox(height: 10),
-            nombreAcField,
-            const SizedBox(height: 10),
-            CURPField,
-            const SizedBox(height: 10),
-            domicilioField,
-            //--------------Pruebas Flutter
-            TextButton(
-              onPressed: () {
-                //-------------Toast
-                Fluttertoast.showToast(
-                    msg: initialList,
-                    toastLength: Toast.LENGTH_SHORT,
-                    gravity: ToastGravity.CENTER,
-                    timeInSecForIosWeb: 1,
-                    backgroundColor: Colors.red,
-                    textColor: Colors.white,
-                    fontSize: 16.0);
-                //-------------Toast
-              },
-              child: const Text('Buscar'),
-            ),
-          ],
-        ),
+          //--------------Pruebas Flutter
+          TextButton(
+            onPressed: () {
+              //-------------Toast
+              Fluttertoast.showToast(
+                  msg: initialList,
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.red,
+                  textColor: Colors.white,
+                  fontSize: 16.0);
+              //-------------Toast
+            },
+            child: const Text('Buscar'),
+          ),
+        ],
+      ),
     );
   }
 }
