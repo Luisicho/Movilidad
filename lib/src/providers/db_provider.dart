ff6f291484e842a4719bd967260aeab9c39b39ac
luisicho <luism18.1999@gmail.com>
2022-12-03 11:21:33 -0700

    Metodo creacion de base de datos ejemplo

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
@@ -19,6 +23,28 @@ class DBProvider {
 
     return _database;
   }
+
   //Retorna Nueva instancia de DB
-  Future<Database?> initDB() async{}
+  Future<Database?> initDB() async {
+    //Path de donde almacenar la base de datos
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'ScansDB.db');
+    print(path);
+
+    //Crear base de datos
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db) {},
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE LEVANTAMIENTO(
+            ID INTEGER PRIMARY KEY,
+            TIPO TEXT,
+            VALOR TEXT
+          );
+        ''');
+      },
+    );
+  }
 }
