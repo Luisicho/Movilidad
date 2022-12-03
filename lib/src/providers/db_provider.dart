83de50404ddb5fe667617fc719c5433f267edc34
luisicho <luism18.1999@gmail.com>
2022-12-03 12:53:29 -0700

    Insert levantamiento

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
-
 import 'package:path/path.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:movilidad/src/pages/levantamiento/model/levantamiento_model.dart';
+export 'package:movilidad/src/pages/levantamiento/model/levantamiento_model.dart';
 
 class DBProvider {
   //Instancia de base de datos
@@ -46,5 +48,29 @@ class DBProvider {
         ''');
       },
     );
+  }
+
+  Future<int?> nuevoLevantamiento(LevantamientoModel nuevoLevantamiento) async {
+    final db = await database;
+    final res = db?.insert('LEVANTAMIENTO', nuevoLevantamiento.toJson());
+    return res;
+  }
+
+  //Insercion Manual RAW
+  Future<int?> nuevoLevatamientoRaw(
+      LevantamientoModel nuevoLevantamiento) async {
+    //Extraer caracteristicas de levantamiento
+    String id = nuevoLevantamiento.Folio;
+    String tipo = nuevoLevantamiento.Descripcion;
+    String valor = nuevoLevantamiento.FechaLlegada;
+    //verificar la base de datos
+    final db = await database;
+    //Insercion
+    final res = await db?.rawInsert('''
+      INSERT INTO LEVANTAMIENTO(ID,TIPO,VALOR)
+        VALUES ('$id','$tipo','$valor')
+    ''');
+
+    return res;
   }
 }
